96d1d76ba31c1b8c1a4534762b9f7916cac7cd50
Vladymyr <tvvist@ukr.net>
2023-12-03 20:53:38 +0200

    add tubs

--- src/js/script.js
+++ src/js/script.js
@@ -27,26 +27,21 @@ const slider = tns({
   slideBy: "page",
   controls: false,
   nav: false,
-  // controlsContainer: Node,
-  // edgePadding: 20,
-  // gutter: 20,
 
-  // responsive:
-  //   {
-  //     640: {
-  //       edgePadding: 20,
-  //       gutter: 20,
-  //       items: 2,
-  //     },
-  //     700: {
-  //       gutter: 30,
-  //     },
-  //     900: {
-  //       items: 3,
-  //     },
-  //   } | true,
-
-  // controlsText: ['<img src="icons/left.svg">', '<img src="icons/right.svg">'],
+  responsive: {
+    320: {
+      edgePadding: 20,
+      gutter: 20,
+      items: 1,
+      nav: true,
+    },
+    767: {
+      nav: true,
+    },
+    845: {
+      nav: false,
+    },
+  },
 });
 
 document.querySelector(".prev").addEventListener("click", function () {
@@ -55,3 +50,39 @@ document.querySelector(".prev").addEventListener("click", function () {
 document.querySelector(".next").addEventListener("click", function () {
   slider.goTo("next");
 });
+
+(function ($) {
+  $(function () {
+    $("ul.catalog__tabs").on(
+      "click",
+      "li:not(.catalog__tab_active)",
+      function () {
+        $(this)
+          .addClass("catalog__tab_active")
+          .siblings()
+          .removeClass("catalog__tab_active")
+          .closest("div.container")
+          .find("div.catalog__content")
+          .removeClass("catalog__content_active")
+          .eq($(this).index())
+          .addClass("catalog__content_active");
+      }
+    );
+
+    function toggleSlide(item) {
+      $(item).each(function (i) {
+        $(this).on("click", function (e) {
+          e.preventDefault();
+          $(".catalog-item__content")
+            .eq(i)
+            .toggleClass("catalog-item__content_active");
+          $(".catalog-item__list")
+            .eq(i)
+            .toggleClass("catalog-item__list_active");
+        });
+      });
+    }
+    toggleSlide(".catalog-item__link");
+    toggleSlide(".catalog-item__back");
+  });
+})(jQuery);
