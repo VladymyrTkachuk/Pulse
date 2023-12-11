f5ff13c5610db81fdaa05826f2a10d20c9ffcc31
Vladymyr <tvvist@ukr.net>
2023-12-11 20:51:59 +0200

    scroll POST

--- src/js/script.js
+++ src/js/script.js
@@ -1,26 +1,3 @@
-// $(document).ready(function () {
-//   $(".carousel__inner").slick({
-//     speed: 1200,
-//     adaptiveHeight: true,
-
-//     arrows: false,
-//     prevArrow:
-//       '<button type="button" class="slick-prev"><img src="icons/left.svg"> </button>',
-//     nextArrow:
-//       '<button type="button" class="slick-next"><img src="icons/right.svg"> </button>',
-
-//     responsive: [
-//       {
-//         breakpoint: 768,
-//         settings: {
-//           dots: true,
-//           arrows: false,
-//         },
-//       },
-//     ],
-//   });
-// });
-
 const slider = tns({
   container: ".carousel__inner",
   items: 1,
@@ -84,5 +61,94 @@ document.querySelector(".next").addEventListener("click", function () {
     }
     toggleSlide(".catalog-item__link");
     toggleSlide(".catalog-item__back");
+
+    // Modal
+
+    $('[data-model= "consultation"]').on("click", function () {
+      $(".overlay, #consultation").fadeIn("slow");
+    });
+
+    $(".modal__close").on("click", function () {
+      $(".overlay, #consultation, #thanks, #order").fadeOut("slow");
+    });
+
+    $(".button_mini").each(function (i) {
+      $(this).on("click", function () {
+        $("#order .modal__descr").text(
+          $(".catalog-item__subtitle").eq(i).text()
+        );
+        $(".overlay, #order").fadeIn("slow");
+      });
+    });
+
+    function valideForms(form) {
+      $(form).validate({
+        rules: {
+          name: {
+            required: true,
+            minlength: 2,
+          },
+
+          phone: "required",
+          email: {
+            required: true,
+            email: true,
+          },
+        },
+
+        messages: {
+          name: {
+            required: "Пожалуйста, введите свое имя",
+            minlength: jQuery.validator.format(" Введите   {0} символов!"),
+          },
+          phone: "Пожалуйста, введите свой номер телефона",
+          email: {
+            required: "Пожалуйста, введите свою почту",
+            email: "Неправильно введен адрес почты",
+          },
+        },
+      });
+    }
+    valideForms("#consultation-form");
+    valideForms("#consultation form");
+    valideForms("#order form");
+
+    $("input[name=phone]").mask("+3(999) 999-99-99");
+
+    $("form").submit(function (e) {
+      e.preventDefault();
+
+      if (!$(this).valid()) {
+        return;
+      }
+
+      $.ajax({
+        type: "POST",
+        url: "mailer/smart.php",
+        data: $(this).serialize(),
+      }).done(function () {
+        $(this).find("input").val("");
+        $("#consultation").fadeOut();
+        $(".overlay, #thanks").fadeIn("slow");
+
+        $("form").trigger("reset");
+      });
+      return false;
+    });
+
+    // Smoss scroll and pageup
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 1600) {
+        $(".pageup").fadeIn();
+      } else {
+        $(".pageup").fadeOut();
+      }
+    });
+
+    $("a[href^='#']").click(function () {
+      const _href = $(this).attr("href");
+      $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
+      return false;
+    });
   });
 })(jQuery);
